454f4100d839a0168c87d5f7d003ad9236eaacc0
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2019-11-05 12:37:28 -0800

    Add confirmation message upon form submit

--- app/main.js
+++ app/main.js
@@ -1,3 +1,12 @@
+const popup = document.querySelector('.modal');
+const button = document.querySelector('button');
+const form = document.querySelector('#survey-form');
 
+//setTimeout(location.reload.bind(location), 160000);
 
-<p>Thank you for submitting! Your response is highly appreciated.</p>
+form.addEventListener("submit", function(e){
+    //alert('Thank you for your submission');
+    popup.style.display = "block";
+    e.preventDefault();
+    setTimeout(function(){window.location.reload(true);},7000)
+})
