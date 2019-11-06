a5764b37965bf88f41cbd48fff757052088f702b
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2019-11-06 10:31:49 -0800

    Clean up code

--- app/main.js
+++ app/main.js
@@ -1,12 +1,9 @@
 const popup = document.querySelector('.modal');
-const button = document.querySelector('button');
 const form = document.querySelector('#survey-form');
 
-//setTimeout(location.reload.bind(location), 160000);
-
+// allows confirmation to appear after all fields have been entered correctly
 form.addEventListener("submit", function(e){
-    //alert('Thank you for your submission');
     popup.style.display = "block";
     e.preventDefault();
-    setTimeout(function(){window.location.reload(true);},7000)
+    setTimeout(function(){window.location.reload(true);},7000) //gives wait time for message before page reload
 })
